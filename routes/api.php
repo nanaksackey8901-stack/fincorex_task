affeed1b06a8604b893282ae1c4575a277bf18db
8901Nanakwame <nanaksackey8901@gmail.com>
2026-05-03 06:17:27 +0000

    api

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+route::prefix('v1')->group(function () {
+
+    Route::get('/test', function () {
+        return response()->json([
+            'message' => 'api test',
+        ], 200);
+    });
+
+});
+
+
+
